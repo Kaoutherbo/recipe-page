933f0a2e9812755907373adb0b9acfa47d3bc758
kaouther <kaoutherboutheldja@gmail.com>
2024-03-29 21:24:31 +0100

    Deploy the app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Card from './components/UI/card'
 import Image from './components/images/image-omelette.jpeg'
 function App() {
   return (
-    <div className="bg-neutral-eggshell flex justify-center items-center h-screen font-medium font-sans">
+    <div className="app bg-neutral-eggshell flex justify-center items-center h-screen font-medium font-sans">
       <Card 
       image= {Image} 
       />
